b7adb9a79d567f3ff18642bb221747e88f860187
dp-246669 <dp-246669@rwu.de>
2025-12-27 23:27:24 +0530

    Second commit on C

--- C/main.c
+++ C/main.c
@@ -1,3 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+int main(int argc, char *argv[])
+{
+    // create two arrays we care about
+    int ages[] = {23, 43, 12, 89, 2};
+    char *names[] = {
+        "Alan", "Frank",
+        "Mary", "John", "Lisa"};
+    // safely get the size of ages
+    int count = sizeof(ages) / sizeof(int);
+    int i = 0;
+    // first way using indexing
+    for (i = 0; i < count; i++)
+    {
+        printf("%s has %d years alive.\n", names[i], ages[i]);
+    }
+    printf("---\n");
+    // set up the pointers to the start of the arrays
+    int *cur_age = (int *)names; // can be buggy!!
+    char **cur_name = names;     // Das ist ein zeiger auf zeiger auf zeichen konzept
+    // second way using pointers
+    for (i = 0; i < count; i++)
+    {
+        printf("%s is %d years old.\n", *(cur_name + i), *(cur_age + i));
+    }
+    printf("---\n");
+    // using arrays
+    for (i = 0; i < count; i++)
+    {
+        printf("%s is %d years old again!\n", cur_name[i], cur_age[i]);
+    }
+    printf("---\n");
+    // 4th really dumb way but really optimized!
+    for (cur_name = names, cur_age = ages;
+         (cur_age - ages) < count; cur_name++, cur_age++)
+    {
+        printf("%s has lived %d years so far!\n", *cur_name, *cur_age);
+    }
+    int height = 100;
+    int *cur_height = &height; // das bedeutet dasselbe!!
+    printf("%d height and %d cur_height", height, *cur_height);
+    return 0;
+}
