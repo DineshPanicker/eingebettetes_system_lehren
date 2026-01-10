894a3a7aebf8c4277b7f2d03db58b287dc066c28
Dinesh Panicker <48146418+DineshPanicker@users.noreply.github.com>
2026-01-10 12:35:55 +0530

    Philips Interview -> Single node creation
    This was the 2nd interview question asked by philips stuttgart for internship

---
+++ C/SingleNode.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node{
+  int data;
+  struct Node *next;
+}Node;
+
+
+int main()
+{
+
+  Node *node1 = malloc(sizeof(Node));
+  if(node1 == NULL) //null ptr check
+    printf("Illegal operation. Abort!\n");
+  
+  node1->data = 43;
+  node1->next = NULL;
+
+  printf("Node value is : %d\n",node1->data);
+
+  free(node1);
+  return 0;
+}
