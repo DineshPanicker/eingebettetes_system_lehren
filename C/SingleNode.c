b90b5663263bc1e24f98028de29cf7e59483663e
dp-246669 <dp-246669@rwu.de>
2026-01-12 12:02:50 +0100

    philips interview questions -> modified to increase readability

--- C/SingleNode.c
+++ C/SingleNode.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node{
+typedef struct Node
+{
   int data;
   struct Node *next;
-}Node;
-
+} Node;
 
 int main()
 {
 
   Node *node1 = malloc(sizeof(Node));
-  if(node1 == NULL) //null ptr check
+  if (node1 == NULL) // null ptr check
     printf("Illegal operation. Abort!\n");
-  
+
   node1->data = 43;
   node1->next = NULL;
 
-  printf("Node value is : %d\n",node1->data);
+  printf("Node value is : %d\n", node1->data);
 
   free(node1);
   return 0;
--- CPP/arr_monotonic.cpp
+++ CPP/arr_monotonic.cpp
@@ -2,46 +2,49 @@
 #include <vector>
 
 using namespace std;
-bool isMonotonic(vector<int>& arr)
+bool isMonotonic(vector<int> &arr)
 {
-  int N = arr.size();
-  bool inc = true;
-  bool dec = true;
-  
-  for(int i = 0;i < N-1;i++)
-{
-  if(arr[i] > arr[i + 1]){
-      inc = false;
-  }
-  if(arr[i] < arr[i + 1]){
-      dec = false;  
-}
-}
-return inc || dec;  
+    int N = arr.size();
+    bool inc = true;
+    bool dec = true;
+
+    for (int i = 0; i < N - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            inc = false;
+        }
+        if (arr[i] < arr[i + 1])
+        {
+            dec = false;
+        }
+    }
+    return inc || dec;
 }
 
-int main() {
-   vector<int>arr1 = {1,5,2};
-   vector<int>arr2 = {1,3,5};
-   vector<int>arr3 = {5,2,1};
-  // Function call
+int main()
+{
+    vector<int> arr1 = {1, 5, 2};
+    vector<int> arr2 = {1, 3, 5};
+    vector<int> arr3 = {5, 2, 1};
+    // Function call
     bool ans1 = isMonotonic(arr1);
     if (ans1)
-        cout << "arr1 is monotonic"<<endl;
+        cout << "arr1 is monotonic" << endl;
     else
-        cout << "arr1 is not monotonic"<<endl;
+        cout << "arr1 is not monotonic" << endl;
 
-  bool ans2 = isMonotonic(arr2);
+    bool ans2 = isMonotonic(arr2);
     if (ans2)
-        cout << "arr2 is monotonic"<<endl;
+        cout << "arr2 is monotonic" << endl;
     else
-        cout << "arr2 is not monotonic"<<endl;
+        cout << "arr2 is not monotonic" << endl;
 
     bool ans3 = isMonotonic(arr3);
     if (ans3)
-        cout << "arr3 is monotonic"<<endl;
+        cout << "arr3 is monotonic" << endl;
     else
-        cout << "arr3 is not monotonic"<<endl;
+        cout << "arr3 is not monotonic" << endl;
 
-return 0;
+    return 0;
 }
