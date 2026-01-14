d00ec302463477c55e7a5ed7b6d2691353127aaa
dp-246669 <dp-246669@rwu.de>
2026-01-14 23:17:20 +0100

    passing array as parameter->DSA-CPP

---
+++ CPP/array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int *fun(int size)
+{
+    int *p;
+    p = new int[size]; // Das Array wird im Heap-Speicher erstellt.
+
+    for (int i = 0; i < size; i++)
+    {
+        p[i] = i + 1;
+    }
+    return p;
+}
+
+int main()
+{
+    int *ptr, sz = 9;
+
+    ptr = fun(sz);
+
+    for (int i = 0; i < sz; i++)
+    {
+        cout << ptr[i] << endl;
+    }
+    return 0;
+}
